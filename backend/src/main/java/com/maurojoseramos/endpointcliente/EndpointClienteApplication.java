175781c45ff82406bdea52443ae8fa5fe8579ee5
MauroJRamos <mauroramosti@gmail.com>
2021-09-27 14:31:11 -0300

    Backend exercicio endpont cliente

---
+++ backend/src/main/java/com/maurojoseramos/endpointcliente/EndpointClienteApplication.java
@@ -0,0 +1,13 @@
+package com.maurojoseramos.endpointcliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndpointClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EndpointClienteApplication.class, args);
+	}
+
+}
